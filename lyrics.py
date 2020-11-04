faab2f8e10dfcc53d9bc2c9abef741118a18f1f9
Lautaro Bachmann <laurigbach@gmail.com>
2020-11-04 13:22:27 -0300

    Changed func to accept author in query

--- lyrics.py
+++ lyrics.py
@@ -3,8 +3,8 @@ from config import genius_api_key
 
 genius = lyricsgenius.Genius(genius_api_key)
 
-def get_lyrics_list(song_title):
-    lyrics = genius.search_song(song_title).lyrics
+def get_lyrics_list(song_title, author):
+    lyrics = genius.search_song(song_title, artist=author).lyrics
     cleaned_lyrics = lyrics
     for rep in ['\n', '\u2005', "'s", "'", '?', ',']:
         cleaned_lyrics = cleaned_lyrics.replace(rep, ' ')
@@ -12,6 +12,6 @@ def get_lyrics_list(song_title):
     return [word.strip() for word in lyrics_set if word and '[' not in word and ']' not in word]
 
 if __name__ == "__main__":
-    for i in get_lyrics_list("Military fashion show"):
+    for i in get_lyrics_list("Military fashion show", 'And One'):
         print(i)
     # get_lyrics_list("Military fashion show")
