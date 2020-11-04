cf19ea0d7d251d2a7196f86cf1d5b0256be0e033
Lautaro Bachmann <laurigbach@gmail.com>
2020-11-04 13:27:47 -0300

    Added '!' to list of characters to replace

--- lyrics.py
+++ lyrics.py
@@ -6,7 +6,7 @@ genius = lyricsgenius.Genius(genius_api_key)
 def get_lyrics_list(song_title, author):
     lyrics = genius.search_song(song_title, artist=author).lyrics
     cleaned_lyrics = lyrics
-    for rep in ['\n', '\u2005', "'s", "'", '?', ',']:
+    for rep in ['\n', '\u2005', "'s", "'", '?', ',', '!']:
         cleaned_lyrics = cleaned_lyrics.replace(rep, ' ')
     lyrics_set = set(cleaned_lyrics.split(' '))
     return [word.strip() for word in lyrics_set if word and '[' not in word and ']' not in word]
