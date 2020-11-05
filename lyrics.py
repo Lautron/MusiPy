3e5099f0185e25c280f5331fab401c35bed4599f
Lautaro Bachmann <laurigbach@gmail.com>
2020-11-05 10:38:52 -0300

    Started using a regex to remove unwanted verses

--- lyrics.py
+++ lyrics.py
@@ -1,20 +1,16 @@
-import lyricsgenius, re, pyperclip
+import lyricsgenius, re, pyperclip, pprint
 from config import genius_api_key
 
 genius = lyricsgenius.Genius(genius_api_key)
 
 def get_lyrics_list(song_title, author):
     lyrics = genius.search_song(song_title, artist=author).lyrics
-    cleaned_lyrics = lyrics.replace('\u2005', ' ')
-    # for rep in ['\n', '\u2005', "'s", "'", '?', ',', '!']:
-    #     cleaned_lyrics = cleaned_lyrics.replace(rep, ' ')
+    regx = re.compile(r'\[.*\]|')
+    cleaned_lyrics = re.sub(regx, '', lyrics.replace('\u2005', ' '))
     lyrics_set = set(cleaned_lyrics.split('\n'))
-    is_allowed = lambda verse: 'Chorus' not in verse and 'Verse' not in verse
-    return [verse.strip() for verse in lyrics_set if is_allowed(verse)]
+    pprint.pprint(cleaned_lyrics.split('\n'))
+    return [verse.strip() for verse in lyrics_set if verse]
 
 if __name__ == "__main__":
-    # for i in get_lyrics_list("Military fashion show", 'And One'):
-    #     print(i)
-    for i in get_lyrics_list("Chorus", 'Erasure'):
+    for i in get_lyrics_list("ZEIG DICH (traducción al español)", 'Rammstein'):
         print(i)
-    # get_lyrics_list("Military fashion show")
