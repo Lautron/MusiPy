bff64877227c9d840072009dc659cb485619b689
Lautaro Bachmann <laurigbach@gmail.com>
2020-11-07 17:12:15 -0300

    Modified func to recieve trans obj through params

--- translate.py
+++ translate.py
@@ -1,11 +1,17 @@
-import googletrans
 from googletrans import Translator
+import time
 
-def translate_verse(verse, desti='es'):
-    trans = Translator()
-    try:
-        translation = trans.translate(verse, dest=desti)
-    except:
-        return 'N/A'
+def translate_verse(verse, trans, desti='es'):
+    translation = None
+    while translation == None:
+        try:
+            translation = trans.translate(verse, dest=desti)
+        except Exception as e:
+            #TODO find a better way to translate verses
+            print(f'\nVerse: {verse}\nException: {e}')
+            return 'N/A'
+            # trans = Translator()
+            # time.sleep(0.5)
+            # pass
 
     return translation.text
