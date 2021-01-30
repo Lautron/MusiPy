718e05453d4eda1e2d36f3b5a7179a97e57e5b54
Lautaro Bachmann <laurigbach@gmail.com>
2021-01-30 09:09:52 -0300

    Simplified translation function

--- translate.py
+++ translate.py
@@ -1,19 +1,8 @@
 from google_trans_new import google_translator
 import time
 
-def translate_verse(verse, trans, desti='es'):
-    translation = None
-    while translation == None:
-
-        translation = trans.translate(verse, lang_tgt=desti)
-        try:
-            pass
-        except Exception as e:
-            #TODO find a better way to translate verses
-            print(f'\nVerse: {verse}\nException: {e}')
-            return 'N/A'
-            # trans = Translator()
-            # time.sleep(0.5)
-            # pass
+def translate_verse(verse, desti='es'):
+    trans = google_translator(timeout=5)
+    translation = trans.translate(verse, lang_tgt=desti)
 
     return translation
