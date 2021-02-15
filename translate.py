33df402e82a934a5ff6ba586d508cb2046f97936
Lautaro Bachmann <laurigbach@gmail.com>
2021-02-15 06:42:42 -0300

    Changed default language to English

--- translate.py
+++ translate.py
@@ -1,7 +1,7 @@
 from google_trans_new import google_translator
 import time
 
-def translate_verse(verse, desti='es'):
+def translate_verse(verse, desti='en'):
     trans = google_translator(timeout=5)
     translation = trans.translate(verse, lang_tgt=desti)
 
